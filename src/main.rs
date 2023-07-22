a050a2b45e4078a7ae65ce6b59b211595e3f6f28
li_tian_ming <2793141823@qq.com>
2023-07-22 18:04:05 +0800

    tests

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+use std::{io, net::TcpListener};
+use zero2prod::run;
+
+#[tokio::main]
+async fn main() -> Result<(),io::Error>{
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+
+    run(listener)?.await
 }
