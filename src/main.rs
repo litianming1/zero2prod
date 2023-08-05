4ebc53393458dd37f394d5dca3456a6b276dbca0
li_tian_ming <2793141823@qq.com>
2023-08-05 18:10:46 +0800

    commit docker

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 use secrecy::ExposeSecret;
-use sqlx::PgPool;
+use sqlx::postgres::PgPoolOptions;
 use std::{io, net::TcpListener};
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
 use zero2prod::telemetry::{get_subscriber, init_subscriber};
+
 
 #[tokio::main]
 async fn main() -> Result<(), io::Error> {
@@ -11,11 +12,13 @@ async fn main() -> Result<(), io::Error> {
     init_subscriber(subscriber);
 
     let configuration = get_configuration().expect("Failed to read configuration");
-    let connection_pool =
-        PgPool::connect(&configuration.database.connection_string().expose_secret())
-            .await
-            .expect("Failed to connect to Postgres");
-    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let connection_pool = PgPoolOptions::new()
+    .acquire_timeout(
+        std::time::Duration::from_secs(2)
+    )
+    .connect_lazy_with(configuration.database.with_db());
+
+    let address = format!("{}:{}", configuration.application.host,configuration.application.port);
     let listener = TcpListener::bind(address)?;
 
     run(listener, connection_pool)?.await?;
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -117,14 +117,14 @@ async fn spawn_app() -> TestApp {
 
 pub async fn configure_database(config: &DatabaseSettings) -> PgPool {
     let mut connection =
-        PgConnection::connect(&config.connection_string_without_db().expose_secret())
+        PgConnection::connect_with(&config.without_db())
             .await
             .expect("Failed to connect to Postgres.");
     connection
         .execute(format!(r#"CREATE DATABASE "{}";"#, config.database_name).as_str())
         .await
         .expect("Failed to create database.");
-    let connection_pool = PgPool::connect(&config.connection_string().expose_secret())
+    let connection_pool = PgPool::connect_with(config.with_db())
         .await
         .expect("Failed to connect to Postgres.");
     sqlx::migrate!("./migrations")
