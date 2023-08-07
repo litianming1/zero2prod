c9a41938b1461f76aa214940641f87c4ee887212
li_tian_ming <2793141823@qq.com>
2023-08-07 18:15:48 +0800

    commit Reject Invalid Subscribers

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -2,6 +2,7 @@ use actix_web::{web, HttpResponse};
 use chrono::Utc;
 use sqlx::PgPool;
 use uuid::Uuid;
+use unicode_segmentation::UnicodeSegmentation;
 
 #[derive(Debug, serde::Deserialize)]
 pub struct FormData {
@@ -14,6 +15,9 @@ pub struct FormData {
     fields(request_id = %Uuid::new_v4(), subscriber_email = %form.email, subscriber_name = %form.name)
 )]
 pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
+   if !is_value_name(&form.name) {
+       return HttpResponse::BadRequest().finish();
+   }
     // 执行插入订阅操作
     match insert_subscriber(&pool, &form).await {
         Ok(_) => HttpResponse::Ok().finish(),
@@ -42,4 +46,14 @@ pub async fn insert_subscriber(pool: &PgPool, form: &FormData) -> Result<(), sql
         e
     })?;
     Ok(())
+}
+
+pub fn is_value_name(s: &str) -> bool {
+    let is_empty_or_whitespace = s.trim().is_empty();
+    let is_too_long = s.graphemes(true).count() > 256;
+    let forbidden_characters = ['/', '(', ')', '"', '<', '>', '\\', '{', '}'];
+    let contains_forbidden_characters = s
+    .chars()
+    .any(|g| forbidden_characters.contains(&g));
+    !(is_empty_or_whitespace || is_too_long || contains_forbidden_characters)
 }
